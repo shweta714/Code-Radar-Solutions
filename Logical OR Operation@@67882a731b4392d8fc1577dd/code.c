86940ae19775f80e0481fa972461729b08d4d816
shweta714 <jha299869@gmail.com>
2025-02-06 11:57:53 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-    int firstnum,sceondnum;
+    int firstnum,secondnum;
     scanf("%d %d",&firsnum,&secondnum);
     if(firstum>0||secondnum>0){
         printf("True\n");
